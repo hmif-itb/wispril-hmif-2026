1d6dd5bd85603b6942fa2b5e53ba4375a5a5a757
Ariel Sitorus <arielsitorus504@gmail.com>
2026-04-17 18:46:54 +0700

    feat: Tampilan Landing Page awal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { SignInButton, SignOutButton } from "~/app/_components/auth-buttons";
 import { LatestPost } from "~/app/_components/post";
 import { getSession } from "~/server/better-auth/server";
 import { api, HydrateClient } from "~/trpc/server";
 import { Rajdhani } from "next/font/google";
+
+const rajdhani = Rajdhani({ subsets: ["latin"], weight: ["400","500","600","700"]});
 
 export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
@@ -16,9 +19,25 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center ">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          HMIF ITB 2026
+      <main className="flex min-h-screen flex-col items-center justify-center bg-[#0d0f12]">
+        <div className="container flex flex-col items-center justify-center px-4 py-16">
+          <Image 
+            src="/logo.svg" 
+            alt="Logo Utama" 
+            width={281} height={265} 
+            className="w-[120px] md:w-[180px] h-auto z-10" />
+
+          <Image 
+            src="/wispril-text.webp" 
+            alt="Wispril Text" 
+            width={656} 
+            height={176} 
+            className="z-20 w-[240px] md:w-[380px] h-auto mt-3 md:mt-5" 
+            />
+
+          <p className={`${rajdhani.className} text-[#EAEA00] text-lg md:text-2xl font-medium tracking-[0.2em] mt-5 md:mt-6`}>
+            HMIF 2026
+          </p>
         </div>
       </main>
     </HydrateClient>
